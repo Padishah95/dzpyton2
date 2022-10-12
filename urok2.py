10b6ff4720bcd9c9db8780b92b9226b8beccfacd
Падишах Павлик <awxo@mail.ru>
2022-10-12 12:02:24 +0500

    three commit

--- urok2.py
+++ urok2.py
@@ -16,3 +16,19 @@
    
 #print(f'Сумма чисел равна {summa(x)}')
 
+# Напишите программу, которая
+# принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+
+#def factorial (number, count = 1):
+    #for i in range(1, number + 1):
+        #count *= i
+    #return count
+
+#n = int(input('Введите цифру: '))
+#print(f'Набор числовых произведенний от 1 {n} = ', end = '')
+#for i in range(1, n + 1):
+    #if i == n: 
+        #print(f'{factorial(i)}')
+    #else:
+        #print(f'{factorial(i)}', end = ', ')
